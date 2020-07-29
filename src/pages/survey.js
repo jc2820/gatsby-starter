7e320fb68a1db9245ed20a54cf94867c6eee8a35
Jamie Carter <jc2820@hotmail.com>
2020-07-29 17:37:59 +0100

    add test survey ready for cms

--- src/pages/survey.js
+++ src/pages/survey.js
@@ -7,16 +7,16 @@ import SEO from "../components/seo"
 
 const IndexPage = props => {
   const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
-
+    console.log({data});
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>{data.title}</h1>
+      {/* <h1>{data.title}</h1>
       <p>{data.intro}</p>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <img src={data.image} alt="photo of Ruby" />
-      </div>
-      <Link to="/survey/">Survey</Link> <br />
+      </div> */}
+      <Link to="/">Go home</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </Layout>
   )
@@ -26,15 +26,15 @@ export default IndexPage
 export const query = graphql`
   query {
     allFile(
-      filter: { sourceInstanceName: { eq: "content" }, name: { eq: "home" } }
+      filter: { sourceInstanceName: { eq: "content" }, name: { eq: "survey" } }
     ) {
       edges {
         node {
           childMarkdownRemark {
             frontmatter {
-              title
-              intro
-              image
+              leaders
+              teachers
+              pupils
             }
           }
         }
