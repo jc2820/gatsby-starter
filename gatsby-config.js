9d498987e48c49d1be3f3b30581d84768091da22
Jamie Carter <jc2820@hotmail.com>
2020-07-29 11:45:25 +0100

    add custom sample data to index

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,21 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: 'images',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    'gatsby-transformer-remark',
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     {
